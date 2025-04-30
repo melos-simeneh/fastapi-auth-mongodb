b610c75e6ff1a8e5adc82c4b8d7a4e820a2524ee
Melos <melos.simeneh@gmail.com>
2025-04-30 14:10:36 +0300

    env vars added

--- utils/settings.py
+++ utils/settings.py
@@ -4,5 +4,8 @@ from os import path
 BASE_DIR = path.dirname(path.dirname(path.realpath(__file__)))
 
 MONGO_URI = config('MONGO_URI')
+MONGO_DB_NAME = config('MONGO_DB_NAME')
+MONGO_TEST_DB_NAME = config('MONGO_TEST_DB_NAME')
+
 SECRET_KEY = config('SECRET_KEY')
 ACCESS_TOKEN_EXPIRE_MINUTES = config('ACCESS_TOKEN_EXPIRE_MINUTES',cast=int)
